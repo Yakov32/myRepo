dba5f0678248774f009bf0918e3b5941350fb650
Yakov32 <yakov.zverev32@gmail.com>
2020-01-18 20:50:14 +0300

    First Commit.

---
+++ class_Chiken.php
@@ -0,0 +1,20 @@
+<?php 
+	class Chiken{
+		private $id;
+		public $regInStable;
+
+		//Тут кол-во яиц у курицы, каждый раз случайно генерируется методом на 15 строке.
+		private $egg;
+		
+		public function __construct($animalId){
+			$this->id = $animalId;
+			$this->regInStable = true;
+		}
+
+		//Метод генерации яйца
+		public function chikensEgg(){
+			$this->egg= rand(0,1);
+			return $this->egg;
+		}
+	}
+ ?>
---
+++ class_Cow.php
@@ -0,0 +1,20 @@
+<?php
+	class Cow{
+		protected $id;
+		public $regInStable;
+		//Тут кол-во молока в корове, кажыдй раз случайно генерируется методом на 14 строке.
+		private $milk;
+
+		public function __construct($animalId){
+			$this->id = $animalId;
+			$this->regInStable = true;
+		}
+
+		//Метод генерации кол-ва молока
+		public function cowsMilk(){
+			$this->milk = rand(8,12);
+			return $this->milk;
+		}
+	}
+
+?>
